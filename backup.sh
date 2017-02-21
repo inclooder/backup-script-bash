f553b6d2dcc607fea7b5cc3b0b2faf870178ecf4
Inclooder <inclooder@gmail.com>
2017-02-21 14:01:38 +0100

    Add more verbosity

--- backup.sh
+++ backup.sh
@@ -6,21 +6,28 @@ set -eo pipefail
 #CONFIGURATION
 DESTINATION_DIR=/var/backups
 TIMESTAMP_FORMAT='+%y-%m-%d.%s'
-BACKUP_DIRS="/var/www"
+BACKUP_DIRS="/var/log"
 BACKUP_MYSQL_DBS="mysql"
-MYSQL_USER=root
-MYSQL_PASS=enter_your_password
+MYSQL_USER="root"
+MYSQL_PASS="enter your password here"
 #CONFIGURATION END
 
-TMP_DIR=`mktemp -d`
+echo "Starting..."
+TMP_DIR="$(mktemp -d)"
+echo "Temp directory created in $TMP_DIR"
 
-for dir in $BACKUP_DIRS; do
-  cp --parents -r "$dir" $TMP_DIR
+for dir in "$BACKUP_DIRS"; do
+  echo "Creating backup for directory $dir"
+  cp --parents -r "$dir" "$TMP_DIR"
 done
 
-for db_name in $BACKUP_MYSQL_DBS; do
-  mysqldump -u $MYSQL_USER -p$MYSQL_PASS $db_name > $TMP_DIR/$db_name.sql
+for db_name in "$BACKUP_MYSQL_DBS"; do
+  echo "Creating dump for database $db_name"
+  mysqldump -u "$MYSQL_USER" -p"$MYSQL_PASS" "$db_name" > "$TMP_DIR/$db_name.sql"
 done
 
-tar -C $TMP_DIR -zcf $DESTINATION_DIR/`date $TIMESTAMP_FORMAT`.tar.gz .
-rm -r $TMP_DIR
+TAR_ARCHIVE="$DESTINATION_DIR/$(date $TIMESTAMP_FORMAT).tar.gz"
+echo "Creating tar archive in $TAR_ARCHIVE"
+tar -C "$TMP_DIR" -zcf "$TAR_ARCHIVE" .
+rm -r "$TMP_DIR"
+echo "Done!"
